424213eeec0b484b4997668db73535381ac76465
신우진 <voltsoftdev@gmail.com>
2022-10-07 23:27:04 +0900

    수업 환경 설정

---
+++ app/src/main/java/com/voltsoft/dev/lecture/IntroActivity.java
@@ -0,0 +1,18 @@
+package com.voltsoft.dev.lecture;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.page_intro);
+
+
+    }
+}
